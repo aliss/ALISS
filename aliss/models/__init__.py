a02e33ec3e5974d3bae42ac2da6c55d77a6143fb
ALISS <technical@aliss.org>
2017-12-06 13:31:38 +0000

    Added model imports to __init__ file

---
+++ aliss/models/__init__.py
@@ -0,0 +1,7 @@
+from .account import ALISSUser
+from .category import Category
+from .organisation import Organisation
+from .service import Service, ServiceArea, ServiceProblem
+from .location import Location
+from .postcode import Postcode
+from .claim import Claim
