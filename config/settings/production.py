f3c869d473162726a71846cc1379a23b9ac7f0b0
ALISS <technical@aliss.org>
2017-12-06 15:57:34 +0000

    Added missing settings for user model, elasticsearch etc

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,8 @@
 from .base import *
 
 DEBUG = False
+
+# Elasticsearch
+ELASTICSEARCH_URL = os.environ.get('FOUNDELASTICSEARCH_URL')
+ELASTICSEARCH_USERNAME = os.environ.get('ELASTICSEARCH_USERNAME')
+ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD')
