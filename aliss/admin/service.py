3694dde523ceb2b33a2374f17f45e781e266d752
ALISS <technical@aliss.org>
2017-12-06 15:56:39 +0000

    Removed slug field from servicearea admin

--- aliss/admin/service.py
+++ aliss/admin/service.py
@@ -6,6 +6,5 @@ from aliss.models import ServiceArea
 @admin.register(ServiceArea)
 class ServiceAreaAdmin(admin.ModelAdmin):
     exclude = ('id',)
-    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'code', 'type')
 
