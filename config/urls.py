6f642c4a437b9acecff2ffce401e8f725a251319
ALISS <technical@aliss.org>
2017-12-06 13:32:50 +0000

    Added aliss app to root urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('aliss.urls')),
 ]
+
