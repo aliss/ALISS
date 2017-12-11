5886c5c007fec847b8c071e3bf641ff09064796d
ALISS <technical@aliss.org>
2017-12-11 11:46:36 +0000

    Added create_superuser method to ALISSUserManager

--- aliss/models/account.py
+++ aliss/models/account.py
@@ -23,6 +23,17 @@ class ALISSUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
 
 class ALISSUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
