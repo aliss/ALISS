4c2ebe08a085175afe0c243bc6ee0e04875ccc38
ALISS <technical@aliss.org>
2018-01-17 10:40:01 +0000

    Added email settings to production settings file

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,3 +9,12 @@ ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD', '')
 
 # Temp session engine until we set up Redis
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
