2522ba94f6cbc4adbc0ed97c3aef6670ef4a3750
ALISS <technical@aliss.org>
2018-01-22 11:49:41 +0000

    Updated production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 # Elasticsearch
-ELASTICSEARCH_URL = os.environ.get('BONSAI_URL')
+ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 ELASTICSEARCH_USERNAME = os.environ.get('ELASTICSEARCH_USERNAME', '')
 ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD', '')
 
