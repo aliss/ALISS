979dfb6768c1b1ae0047f66944132c4ede39fe01
Roryg <giannichan@gmail.com>
2018-04-19 15:26:39 +0100

    better null checking for anon users

--- aliss/models/organisation.py
+++ aliss/models/organisation.py
@@ -37,7 +37,7 @@ class Organisation(models.Model):
     published = models.BooleanField(default=True)
 
     def is_edited_by(self, user):
-        if user == None:
+        if user == None or user.pk == None:
             return False
         return (
             user.is_staff or \
