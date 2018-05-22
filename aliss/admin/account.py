5a6e9a777268e5b8bfa7f64970f921d141594b9c
Roryg <giannichan@gmail.com>
2018-05-22 14:23:24 +0100

    show more details about user from admin

--- aliss/admin/account.py
+++ aliss/admin/account.py
@@ -5,3 +5,4 @@ from aliss.models import ALISSUser
 @admin.register(ALISSUser)
 class ALISSUserAdmin(admin.ModelAdmin):
     search_fields = ['email']
+    list_display = ['email', 'name', 'is_editor', 'is_staff']
