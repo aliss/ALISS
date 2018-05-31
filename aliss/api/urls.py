30fad8ed5741be5fb9758641e33e9551e882380c
Roryg <giannichan@gmail.com>
2018-05-31 18:33:23 +0100

    adds v4 endpoints

--- aliss/api/urls.py
+++ aliss/api/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
-
-from .views import SearchView, CategoryListView, ServiceAreaListView
+from . import views as v3
+from . import v4_views as v4
 
 urlpatterns = [
-    url(r'^v3/search/$', SearchView.as_view()),
-    url(r'^v3/categories/$', CategoryListView.as_view()),
-    url(r'^v3/service-areas/$', ServiceAreaListView.as_view()),
+    url(r'^v3/search/$', v3.SearchView.as_view()),
+    url(r'^v3/categories/$', v3.CategoryListView.as_view()),
+    url(r'^v3/service-areas/$', v3.ServiceAreaListView.as_view()),
+
+    url(r'^v4/search/$', v4.SearchView.as_view()),
+    url(r'^v4/categories/$', v4.CategoryListView.as_view()),
+    url(r'^v4/service-areas/$', v4.ServiceAreaListView.as_view()),
 ]
