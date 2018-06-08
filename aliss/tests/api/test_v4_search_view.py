eb54a3f11c51cdfa588c4efbb2d4e81591af7915
Roryg <giannichan@gmail.com>
2018-06-08 13:17:03 +0100

    adds fixtures and include new tests for apis

--- aliss/tests/api/test_v4_search_view.py
+++ aliss/tests/api/test_v4_search_view.py
@@ -4,12 +4,12 @@ from django.urls import reverse
 from aliss.tests.fixtures import Fixtures
 from aliss.models import *
 
-class v3SearchViewTestCase(TestCase):
+class v4SearchViewTestCase(TestCase):
 
     def setUp(self):
       Fixtures.create()
       self.client = Client()
 
     def test_get(self):
-        response = self.client.get("/api/v3/search/", { 'postcode': 'G2 4AA' }, format="json")
+        response = self.client.get('/api/v4/services/', { 'postcode': 'G2 4AA' }, format="json")
         self.assertEqual(response.status_code, 200)
