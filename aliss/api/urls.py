d450a2e22fffb47e27fd39d5382bc9b2b33c9e94
Roryg <giannichan@gmail.com>
2018-06-08 14:52:55 +0100

    use restful routes

--- aliss/api/urls.py
+++ aliss/api/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^v3/categories/$', v3.CategoryListView.as_view()),
     url(r'^v3/service-areas/$', v3.ServiceAreaListView.as_view()),
 
-    url(r'^v4/search/$', v4.SearchView.as_view()),
+    url(r'^v4/services/$', v4.SearchView.as_view()),
     url(r'^v4/categories/$', v4.CategoryListView.as_view()),
     url(r'^v4/service-areas/$', v4.ServiceAreaListView.as_view()),
 ]
