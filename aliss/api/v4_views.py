28ecdb75f02927fe9d5c50dc4ab94db0b991d2b6
Roryg <giannichan@gmail.com>
2018-06-08 14:53:20 +0100

    adds more meta info and use new serialisers

--- aliss/api/v4_views.py
+++ aliss/api/v4_views.py
@@ -3,22 +3,42 @@ from . import views as v3
 from aliss.models import Category, ServiceArea
 
 from .serializers import (
-    SearchSerializer,
+    v4SearchSerializer,
     SearchInputSerializer,
-    CategorySerializer,
+    v4CategorySerializer,
     ServiceAreaSerializer
 )
 
 class APIv4():
-    META = { 'licence': 'tbc' }
+    META = {
+        'licence': 'https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode',
+        'attribution': [{
+            'text': 'Contains National Statistics data © Crown copyright and database right 2018',
+            'url': 'http://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2016-generalised-clipped-boundaries-in-the-uk/'
+        },{
+            'text': 'Contains information from the Scottish Charity Register supplied by the Office of the Scottish Charity Regulator and licensed under the Open Government Licence v2.0',
+            'url': 'https://www.oscr.org.uk/about-charities/search-the-register/charity-register-download'
+        },{
+            'text': 'Contains National Records of Scotland data licensed under the Open Government Licence v3.0',
+            'url': 'https://www.nrscotland.gov.uk/statistics-and-data/geography/nrs-postcode-extract'
+        },{
+            'text': 'Contains contributions from ALISS users',
+            'url': 'https://www.aliss.org/terms-and-conditions'
+        }]
+    }
 
 
 class SearchView(v3.SearchView):
+    serializer_class = v4SearchSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
     def get(self, request, *args, **kwargs):
         response = self.list(request, *args, **kwargs)
-        data = {}
-        data.update(APIv4.META)
+        data = { 'meta': APIv4.META }
         data.update(response.data)
+        data['data'] = data.pop('results')
         return Response(data)
 
 
@@ -26,15 +46,18 @@ class ServiceAreaListView(v3.ServiceAreaListView):
     def list(self, request):
         queryset = self.get_queryset()
         serializer = ServiceAreaSerializer(queryset, many=True)
-        data = { 'results': serializer.data }
-        data.update(APIv4.META)
+        data = { 'meta': APIv4.META, 'data': serializer.data }
         return Response(data)
 
 
 class CategoryListView(v3.CategoryListView):
+
+    def get_queryset(self):
+        return Category.objects.filter(parent=None)
+
     def list(self, request):
         queryset = self.get_queryset()
-        serializer = CategorySerializer(queryset, many=True)
-        data = { 'results': serializer.data }
-        data.update(APIv4.META)
+        serializer = v4CategorySerializer(queryset, many=True)
+        data = { 'meta': APIv4.META }
+        data.update({'data': serializer.data })
         return Response(data)
