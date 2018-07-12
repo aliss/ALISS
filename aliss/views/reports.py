9ac3d2ac9737de8aae2d5d6ee5b870117a301eea
Roryg <giannichan@gmail.com>
2018-07-12 18:38:30 +0100

    adds filter button and test

--- aliss/views/reports.py
+++ aliss/views/reports.py
@@ -10,6 +10,7 @@ from braces.views import LoginRequiredMixin, StaffuserRequiredMixin
 
 from aliss.models import ALISSUser, Service, ServiceArea, Organisation, RecommendedServiceList, ServiceProblem, Claim
 from datetime import datetime
+import pytz
 
 
 class ReportsView(StaffuserRequiredMixin, TemplateView):
@@ -23,6 +24,7 @@ class ReportsView(StaffuserRequiredMixin, TemplateView):
 
         start_str = self.request.GET.get('start_date_submit',None)
         end_str   = self.request.GET.get('end_date_submit',None)
+        context['filter_unpublished'] = self.request.GET.get('filter_unpublished', '')
 
         context['start_date'] = datetime.now().replace(day=1)
         context['end_date'] = datetime.now()
@@ -32,16 +34,21 @@ class ReportsView(StaffuserRequiredMixin, TemplateView):
         if end_str:
             context['end_date'] = datetime.strptime(end_str, '%Y/%m/%d')
 
-        context['service_count'] = Service.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date']).count()
-        context['orgs_count']    = Organisation.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date']).count()
+        context['start_date'] = context['start_date'].replace(tzinfo=pytz.UTC)
+        context['end_date'] = context['end_date'].replace(tzinfo=pytz.UTC)
+
+        orgs = Organisation.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date'])
+        services = Service.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date'])
+
+        if context['filter_unpublished'] == 'true':
+            orgs = orgs.exclude(published=False)
+            services = services.exclude(organisation__published=False)
+
+        context['orgs_count'] = orgs.count()
+        context['service_count'] = services.count()
+
         context['user_count']    = ALISSUser.objects.filter(date_joined__gte=context['start_date']).filter(date_joined__lte=context['end_date']).count()
         context['problem_count'] = ServiceProblem.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date']).count()
         context['claim_request_count'] = Claim.objects.filter(created_on__gte=context['start_date']).filter(created_on__lte=context['end_date']).count()
-
-        #context['services'] = Service.objects.prefetch_related('locations', 'service_areas').all()
-        #context['service_areas'] = ServiceArea.objects\
-        #    .prefetch_related('services')\
-        #    .order_by('type', 'code')
-        #context['recently_added'] = Service.objects.all().order_by('-created_on')[:10]
 
         return context
