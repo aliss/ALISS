586b80462d22b0295dc0e9052a43065901ce73b2
Roryg <giannichan@gmail.com>
2018-07-12 18:38:30 +0100

    adds filter button and test

--- aliss/tests/views/test_reports_view.py
+++ aliss/tests/views/test_reports_view.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
+from aliss.models import Organisation, ALISSUser, Service, Location
 
 
 class ReportsViewTestCase(TestCase):
+    def setUp(self):
+        self.staff   = ALISSUser.objects.create_user("staff@aliss.org", "passwurd", is_staff=True)
+        self.client.login(username='staff@aliss.org', password='passwurd')
 
     def test_get(self):
-        response = self.client.get('/reports')
+        response = self.client.get('/reports/')
         self.assertEqual(response.status_code, 200)
