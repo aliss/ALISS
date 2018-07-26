254d727a8fd8772e8f814c7fbd8517affcd8c3f9
Roryg <giannichan@gmail.com>
2018-07-26 14:48:31 +0100

    fix param names and adds further test coverage

--- aliss/tests/api/test_v4_search_view.py
+++ aliss/tests/api/test_v4_search_view.py
@@ -12,8 +12,14 @@ class v4SearchViewTestCase(TestCase):
 
     def test_get(self):
         response = self.client.get('/api/v4/services/', { 'postcode': 'G2 4AA' }, format="json")
+        response2 = self.client.get('/api/v4/services/', { 'q': 'money', 'postcode': 'G2 4AA' }, format="json")
+
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response2.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/json')
+        self.assertEqual(response2['Content-Type'], 'application/json')
         self.assertTrue('count' in response.data)
         self.assertTrue('meta' in response.data)
         self.assertTrue('data' in response.data)
+        self.assertTrue(response.data['count'] > 0)
+        self.assertTrue(response.data['count'] > response2.data['count'])
