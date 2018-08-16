03a445ec0ee26881a2907f9a1469d22faf9cd483
R Gianni <hello@rgianni.cc>
2018-08-16 15:11:53 +0100

    punc and stop words stripping for org search plus test

--- aliss/filters.py
+++ aliss/filters.py
@@ -1,16 +1,24 @@
 from django.db.models import Q
 
 import django_filters
+import string
 
 from aliss.models import ALISSUser, Organisation
 
 
 class OrganisationFilter(django_filters.FilterSet):
-    q = django_filters.CharFilter(name='name', lookup_expr='icontains')
+    q = django_filters.CharFilter(name='name', lookup_expr='icontains', method='name_filter')
 
     class Meta:
         model = Organisation
         fields = ['q']
+
+    def name_filter(self, queryset, name, value):
+        puncstripper = str.maketrans('', '', string.punctuation.replace('-', ''))
+        value = value.translate(puncstripper)
+        stopwords = ['the', 'a', 'and', '&']
+        stopped_arr = filter(lambda stopword : stopword not in stopwords, value.split())
+        return queryset.filter(**{ name: " ".join(stopped_arr), })
 
 
 class AccountFilter(django_filters.FilterSet):
