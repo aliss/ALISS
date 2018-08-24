686e0ff48db63c93b7d9a31460c5a5a7226dc451
Mike-Heneghan <m.heneghan@live.co.uk>
2018-08-24 16:44:06 +0100

    Updated the comments on code and improved variable names.

--- aliss/views/account.py
+++ aliss/views/account.py
@@ -428,48 +428,22 @@ class AccountMyDigestView(LoginRequiredMixin, TemplateView):
         utc = pytz.UTC
         current_date = datetime.now()
         current_date = utc.localize(current_date)
+        # Define the number of weeks
+        number_of_weeks = 1
         # Create the historical date to compare against i.e. one week ago
-        comparison_date = current_date - timedelta(minutes=30)
+        comparison_date = current_date - timedelta(weeks=number_of_weeks)
 
         # Iterate through the services and compare the updated_on date with the historical date
+        number_of_services = 3
         count = 0
         for service in updated_in_x_weeks:
             if (service.updated_on > comparison_date):
                 count += 1
+                # Sets the number of records to be iterated through when
                 if count >= 3:
-                    context['updated_services'] = self.request.user.saved_services.all().order_by('updated_on').reverse()[:3]
+                    context['updated_services'] = self.request.user.saved_services.all().order_by('updated_on').reverse()[:number_of_services]
                     return context
             if (service.updated_on < comparison_date):
                 context['updated_services'] = updated_in_x_weeks.exclude(updated_on = service.updated_on)[:count]
                 return context
                 break
-
-
-        # context['updated_services'] = updated_in_x_weeks
-        # return context
-
-        # count = 0
-        # for service in updated_in_x_weeks:
-        #     if count = 3:
-        #         context['updated_services'] = updated_in_x_weeks[:3]
-        #         return context
-        #     if service.updated_on > comparison_date:
-        #         count += 1
-        #     if service.updated_on < comparison_date:
-        #         context['updated_services'] = updated_in_x_weeks.exclude(updated_on = service.updated_on)[:count]
-        #         return context
-        # return context
-
-
-        # context['updated_services'] = updated_in_x_weeks
-        # return context
-        # Trying to filter using date conditional
-        # context['updated_services'] = self.request.user.saved_services.all().filter(updated_on > comparison_date)
-        # return context
-
-
-    # Return 3 most recently updated saved services no dates taked into account
-    # def get_context_data(self, **kwargs):
-    #     context = super(AccountMyDigestView, self).get_context_data(**kwargs)
-    #     context['updated_services'] =self.request.user.saved_services.all().order_by('updated_on').reverse()[:3]
-    #     return context
