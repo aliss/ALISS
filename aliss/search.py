c11f8152b8778d636f7c95dae52c0fdb5fef3523
Mike-Heneghan <m.heneghan@live.co.uk>
2018-08-29 16:09:15 +0100

    Currently unable to filter by updated_on value

--- aliss/search.py
+++ aliss/search.py
@@ -287,7 +287,13 @@ def filter_by_location_type(queryset, type):
     else:
         return queryset
 
-# def sort_by_updated_on(queryset):
-#     queryset = queryset.sort({
-#
-#     })
+def filter_by_updated_on(queryset, comparison_date):
+    queryset = queryset.query({
+        Q("query":{
+            "bool": {
+                "filter": {"range":{"updated_on":{"gte":comparison_date}}}
+            }
+        }
+
+    }))
+    return queryset
