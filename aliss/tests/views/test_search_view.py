17b0a5e09d9e01eefeb74bd502e1f1eadfa3f913
R Gianni <hello@rgianni.cc>
2018-09-06 20:18:04 +0100

    adds content test

--- aliss/tests/views/test_search_view.py
+++ aliss/tests/views/test_search_view.py
@@ -1,10 +1,28 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
-#from aliss.models import Organisation, ALISSUser, Service, Location
+from aliss.models import Organisation, ALISSUser, Service, Location, ServiceArea, Postcode
 
 class SearchViewTestCase(TestCase):
-    #def setUp(self):
+    fixtures = ['service_areas.json', 'g2_postcodes.json']
+
+    def setUp(self):
+        t = ALISSUser.objects.create(name="Mr Test", email="tester@aliss.org")
+        u = ALISSUser.objects.create(name="Mr Updater", email="updater@aliss.org", is_editor=True)
+        o = Organisation.objects.create(
+          name="TestOrg",
+          description="A test description",
+          created_by=t, updated_by=u
+        )
+        s = Service.objects.create(name="My First Service", description="A handy service", organisation=o, created_by=t, updated_by=u)
+        s.service_areas.add(ServiceArea.objects.get(name="Glasgow City", type=2))
+
+    def test_no_postcode(self):
+        response = self.client.get('/search/?postcode=ZZ+ZZZ')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<h1>Sorry, ZZ ZZZ doesn\'t appear to be a valid postcode.</h1>')
+
     def test_get(self):
         response = self.client.get('/search/?postcode=G2+4AA')
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Help &amp; support in <span class="postcode">G2 4AA</span>')
