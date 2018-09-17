b1c7ec63fbc3278177927f746ec625d9bea0e23c
R Gianni <hello@rgianni.cc>
2018-09-17 12:00:47 +0100

    use sitemap index

--- aliss/sitemap.py
+++ aliss/sitemap.py
@@ -5,33 +5,42 @@ from aliss.models import Service, Organisation
 
 
 class ServiceSitemap(Sitemap):
-    changefreq = 'monthly'
+    changefreq = 'daily'
     priority = 0.5
     protocol = 'https'
+    limit = 5000
 
     def items(self):
-        return Service.objects.all()
+        return Service.objects.order_by('updated_on').all()
 
     def lastmod(self, obj):
         return obj.updated_on
 
     def location(self, obj):
-        return reverse('service_detail', kwargs={'pk': obj.pk})
+        if obj.slug:
+            return reverse('service_detail_slug', kwargs={'slug': obj.slug})
+        else:
+            return reverse('service_detail', kwargs={'pk': obj.pk})
 
 
 class OrganisationSitemap(Sitemap):
     changefreq = 'monthly'
     priority = 0.5
     protocol = 'https'
+    limit = 5000
+    section = "static"
 
     def items(self):
-        return Organisation.objects.all()
+        return Organisation.objects.order_by('updated_on').all()
 
     def lastmod(self, obj):
         return obj.updated_on
 
     def location(self, obj):
-        return reverse('organisation_detail', kwargs={'pk': obj.pk})
+        if obj.slug:
+            return reverse('organisation_detail_slug', kwargs={'slug': obj.slug})
+        else:
+            return reverse('organisation_detail', kwargs={'pk': obj.pk})
 
 
 class StaticViewSitemap(Sitemap):
