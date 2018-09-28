0769fe67449c9935dffe84767f618d01116b3788
Mike-Heneghan <m.heneghan@live.co.uk>
2018-09-28 14:27:51 +0100

    Created test for the Digest Selection view copied from the account view.

---
+++ aliss/tests/views/test_account_digest_selection.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from aliss.models import Organisation, ALISSUser, Service, Location
+
+class AccountDigestSelectionViewTestCase(TestCase):
+    def setUp(self):
+        self.user = ALISSUser.objects.create_user("random@random.org", "passwurd")
+        self.client.login(username='random@random.org', password='passwurd')
+
+    def test_digest_selection(self):
+        response = self.client.get(reverse('account_my_digest'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_digest_selection(self):
+        self.client.logout()
+        response = self.client.get(reverse('account_my_digest'))
+        self.assertEqual(response.status_code, 302)
