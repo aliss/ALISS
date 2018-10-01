020c8ae4bbc37a443d6d5c209bfdb936a071b268
Mike-Heneghan <m.heneghan@live.co.uk>
2018-10-01 10:36:39 +0100

    Fixed spelling error in the digest email generator method

--- aliss/tests/views/test_account_digest_selection.py
+++ aliss/tests/views/test_account_digest_selection.py
@@ -12,7 +12,16 @@ class AccountDigestSelectionViewTestCase(TestCase):
         response = self.client.get(reverse('account_my_digest'))
         self.assertEqual(response.status_code, 200)
 
+    def test_create_digest_selection(self):
+        response = self.client.get(reverse('account_create_my_digest'))
+        self.assertEqual(response.status_code, 200)
+
     def test_logout_digest_selection(self):
         self.client.logout()
         response = self.client.get(reverse('account_my_digest'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_logout_create_digest_selection(self):
+        self.client.logout()
+        response = self.client.get(reverse('account_create_my_digest'))
         self.assertEqual(response.status_code, 302)
