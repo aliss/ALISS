d028305605b2ab3e602e96dede8b8e023716e9f2
R Gianni <hello@rgianni.cc>
2018-10-03 11:00:23 +0100

    order by created and adds published for org admin table

--- aliss/admin/organisation.py
+++ aliss/admin/organisation.py
@@ -6,5 +6,5 @@ from aliss.models import Organisation
 @admin.register(Organisation)
 class OrganisationAdmin(admin.ModelAdmin):
     exclude = ('id',)
-    list_display = ('name', 'created_by', 'created_on', 'updated_on')
-
+    list_display = ('name', 'created_by', 'created_on', 'updated_on', 'published')
+    ordering = ('-created_on',)
