0c4ad5b7e37bd52316cb9867734e5a6a88fd0e11
R Gianni <hello@rgianni.cc>
2018-10-03 16:08:32 +0100

    adds services to django admin

--- aliss/admin/service.py
+++ aliss/admin/service.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from aliss.models import ServiceArea
+from aliss.models import ServiceArea, Service
 
 
 @admin.register(ServiceArea)
@@ -8,3 +8,9 @@ class ServiceAreaAdmin(admin.ModelAdmin):
     exclude = ('id',)
     list_display = ('name', 'code', 'type')
 
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+    list_display = ('name', 'organisation', 'created_by', 'created_on', 'updated_on')
+    ordering = ('-created_on',)
