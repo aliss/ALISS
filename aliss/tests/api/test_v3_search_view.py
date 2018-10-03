471f3e9f1246a1085d039b09300c56df761d4b79
R Gianni <hello@rgianni.cc>
2018-10-03 14:12:14 +0100

    refactors models, ensures service index is updated on delete or save

--- aliss/tests/api/test_v3_search_view.py
+++ aliss/tests/api/test_v3_search_view.py
@@ -3,13 +3,11 @@ from django.test import Client
 from django.urls import reverse
 from aliss.tests.fixtures import Fixtures
 from aliss.models import *
-from aliss.search import index_service, delete_service
 
 class v3SearchViewTestCase(TestCase):
 
     def setUp(self):
       self.service = Fixtures.create()
-      index_service(self.service)
       self.client = Client()
 
     def test_get(self):
@@ -22,4 +20,4 @@ class v3SearchViewTestCase(TestCase):
         self.assertTrue('url' in service_result)
 
     def tearDown(self):
-        delete_service(self.service.pk)
+        self.service.delete()
