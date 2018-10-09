ed9e8324c3d456cc59ecea6dedd3183b92e0390b
R Gianni <hello@rgianni.cc>
2018-10-09 15:08:54 +0100

    handles category delete

--- aliss/admin/category.py
+++ aliss/admin/category.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 from django.db import models
 from aliss.models import Category
 
+
+def remove_categories(modeladmin, request, queryset):
+    #makes sure we use overriden delete
+    for category in queryset:
+        category.delete()
+remove_categories.short_description = "Remove selected categories"
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     exclude = ('id',)
     search_fields = ['name']
     list_display = ['name', 'slug', 'service_count', 'subcategory_count']
+    actions = [remove_categories]
 
     def get_queryset(self, request):
         qs = super(CategoryAdmin, self).get_queryset(request)
@@ -20,3 +29,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def subcategory_count(self, obj):
         return Category.objects.filter(parent=obj).count()
+
+    def get_actions(self, request):
+        actions = super(CategoryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
