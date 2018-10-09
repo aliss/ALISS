4d90c907afa2de9ede1790ec0ff5e79cd4ffa5ef
R Gianni <hello@rgianni.cc>
2018-10-09 13:05:48 +0100

    refactor and make sure category changes are indexed properly

--- aliss/tests/views/test_service_view.py
+++ aliss/tests/views/test_service_view.py
@@ -2,9 +2,12 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 from aliss.tests.fixtures import Fixtures
-from aliss.models import Organisation, ALISSUser, Service, Location
+from aliss.models import Organisation, ALISSUser, Service, Location, Category, ServiceArea
+from aliss.search import (get_service)
 
 class ServiceViewTestCase(TestCase):
+    fixtures = ['categories.json', 'service_areas.json']
+
     def setUp(self):
         self.user = ALISSUser.objects.create_user("random@random.org", "passwurd")
         self.client.login(username='random@random.org', password='passwurd')
@@ -18,4 +21,31 @@ class ServiceViewTestCase(TestCase):
     def test_logout_service_create(self):
         self.client.logout()
         response = self.client.get(reverse('service_create', kwargs={'pk':self.organisation.pk}))
+        self.assertEqual(response.status_code, 302)
+
+    def test_service_update_get(self):
+        service = Fixtures.create_service(self.organisation)
+        x=reverse('service_edit', kwargs={ 'pk': service.pk })
+        response = self.client.get(x)
+        self.assertEqual(response.status_code, 200)
+
+    def test_service_valid_update(self):
+        service = Fixtures.create_service(self.organisation)
+        category = Category.objects.first()
+        response = self.client.post(reverse('service_edit', kwargs={ 'pk': service.pk }),{
+            'name': 'an updated service',
+            'description': 'a full description',
+            'categories': [category.pk],
+            'service_areas': [ServiceArea.objects.first().pk]
+        })
+
+        service.refresh_from_db()
+        queryset = Fixtures.es_connection()
+        result = get_service(queryset, service.id)[0]
+        category_count = len(result['categories'])
+
+        self.assertEqual(category_count, 1)
+        self.assertEqual(result['categories'][0]['name'], category.name)
+        self.assertEqual(service.name, 'an updated service')
+        self.assertEqual(service.updated_by, self.user)
         self.assertEqual(response.status_code, 302)
