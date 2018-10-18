a109f5f2bb5604056a552c60a90c0eb41973698a
Mike-Heneghan <m.heneghan@live.co.uk>
2018-10-18 09:26:01 +0100

    Added further tests for checking postcode validity when it is entered.

--- aliss/tests/forms/test_account_form.py
+++ aliss/tests/forms/test_account_form.py
@@ -22,7 +22,32 @@ class SignupFormTestCase(TestCase):
         form = SignupForm(request.POST)
         self.assertEqual(form.is_valid(), True)
 
+    def test_form_is_valis_basic_user_postcode_case_insensitive(self):
+        request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password, 'accept_terms_and_conditions':True, 'postcode':'eH21 6uW' })
+        form = SignupForm(request.POST)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_form_is_valis_basic_user_postcode_whitespace(self):
+        request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password, 'accept_terms_and_conditions':True, 'postcode':' EH21 6UW ' })
+        form = SignupForm(request.POST)
+        self.assertEqual(form.is_valid(), True)
+
     def test_form_is_invalid_basic_user_password_mismatch(self):
         request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password_mismatch, 'accept_terms_and_conditions':True})
+        form = SignupForm(request.POST)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_form_is_invalid_basic_user_terms_not_accepted(self):
+        request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password_mismatch, 'accept_terms_and_conditions':False})
+        form = SignupForm(request.POST)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_form_is_invalid_basic_user_postcode_special_character(self):
+        request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password_mismatch, 'accept_terms_and_conditions':False, 'postcode':"EH2£ 6UW"})
+        form = SignupForm(request.POST)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_form_is_invalid_basic_user_postcode_too_many_characters(self):
+        request = self.factory.post(reverse('signup'), { 'name': self.name, 'email':self.email, 'password1': self.password, 'password2':self.password_mismatch, 'accept_terms_and_conditions':False, 'postcode':"EH21 6UWW"})
         form = SignupForm(request.POST)
         self.assertEqual(form.is_valid(), False)
