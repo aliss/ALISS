15bad2d11e8060f92eb307a6393140ce26caa7a2
Mike-Heneghan <m.heneghan@live.co.uk>
2019-01-14 15:11:34 +0000

    Updated the order of the model init file to stop error with sequence of imports.

--- aliss/models/__init__.py
+++ aliss/models/__init__.py
@@ -1,5 +1,4 @@
 from .aliss_cloudinary import ALISSCloudinaryField
-from .account import ALISSUser, RecommendedServiceList
 from .category import Category
 from .service_area import ServiceArea
 from .organisation import Organisation
