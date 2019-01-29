2a7dd2fa8ad759ec045c0395cc70029ff4b543c1
Mike-Heneghan <m.heneghan@live.co.uk>
2019-01-29 14:39:06 +0000

    Added a url to a copy of the homepage although with a search box for keywords instead of postcodes. Redirects to a blank results page at the moment.

--- aliss/urls/search.py
+++ aliss/urls/search.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from aliss.views import SearchView, SearchOrganisationsView
 
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^organisations/$', SearchOrganisationsView.as_view(), name='organisation_results'),
+    url(r'^organisations/results/$', SearchOrganisationsView.as_view(), name='results'),
+    url(r'^organisations/$', TemplateView.as_view(template_name="search/organisation-search.html"), name='organisations'),
     url(r'^$', SearchView.as_view(), name='search')
 
 ]
