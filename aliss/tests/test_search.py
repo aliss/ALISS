1ac7223f2dc346c67654d948a8fc9eec33d38c5e
R Gianni <hello@rgianni.cc>
2019-02-08 11:19:57 +0000

    rename variables

--- aliss/tests/test_search.py
+++ aliss/tests/test_search.py
@@ -14,18 +14,18 @@ class SearchTestCase(TestCase):
         self.org = Fixtures.create_organisation(t, u, c)
         self.org2 = Organisation.objects.create(name="Test0rg", description="A test description",
             created_by=self.org.created_by, updated_by=self.org.updated_by)
-        close = Fixtures.create_location(self.org)
-        far = Fixtures.create_another_location(self.org)
+        location1 = Fixtures.create_location(self.org)
+        location2 = Fixtures.create_another_location(self.org)
 
         self.s1 = Service.objects.create(name="Food For All", description="A handy food activity", organisation=self.org, created_by=t, updated_by=u)
         self.s2 = Service.objects.create(name="Physical Fun", description="Physical activity classes", organisation=self.org, created_by=t, updated_by=u)
         self.s3 = Service.objects.create(name="Step Fit 1", description="Physical activity classes", organisation=self.org, created_by=t, updated_by=u)
         self.s4 = Service.objects.create(name="Step Fit 2", description="Phyzical activiti classes", organisation=self.org, created_by=t, updated_by=u)
 
-        self.s1.locations.add(close); self.s1.save()
-        self.s2.locations.add(close); self.s2.save()
-        self.s3.locations.add(close); self.s3.save()
-        self.s4.locations.add(far); self.s4.save()
+        self.s1.locations.add(location1); self.s1.save()
+        self.s2.locations.add(location1); self.s2.save()
+        self.s3.locations.add(location1); self.s3.save()
+        self.s4.locations.add(location2); self.s4.save()
 
         pks = [self.s1.pk, self.s2.pk, self.s3.pk, self.s4.pk]
         self.queryset = get_services(Fixtures.es_connection(), pks)
