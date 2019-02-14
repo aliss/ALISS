ccdbe4257f6d132f5fc70011dae44ccbc51724e3
Mike-Heneghan <m.heneghan@live.co.uk>
2019-02-14 16:02:47 +0000

    Successfully outputting db records to a csv file although formatting is not perfect.

--- aliss/management/commands/export_csv.py
+++ aliss/management/commands/export_csv.py
@@ -12,12 +12,17 @@ class Command(BaseCommand):
 
     def write_result_csv(self, collection, filepath='aliss_service.csv'):
         with open(filepath, mode='w') as output_file:
-            csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            csv_writer.writerow(["Name", "Description", "URL", "Phone Number",
+            "Email Address", "URL", "Last Edited", "Organisation ID", "Organisation Name", "Organisation Categories", "Service Areas", "Locations"])
             for obj in collection:
-                csv_writer.writerow([obj.id, obj.name])
+                csv_writer.writerow([obj.name, obj.description, obj.url, obj.phone, obj.email, obj.url, obj.last_edited, obj.organisation_id, obj.organisation.name, obj.categories, obj.service_areas, obj.locations])
 
 
     def handle(self, *args, **options):
         self.stdout.write("\nWriting CSV\n")
         results = Service.objects.all()[:5]
         self.write_result_csv(results)
+
+
+#name, description, url, phone, email, aliss_url, permalink, last_edited, organisation_id, organisation_name, organisation_permalink, categories, service_areas, location_ids
