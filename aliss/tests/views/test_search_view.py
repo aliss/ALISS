da476868a579b6b9bb17e8894eeb32487675f252
Mike-Heneghan <m.heneghan@live.co.uk>
2019-02-28 10:13:43 +0000

    Manually added the brechin, erskine and dundee postcode to the test db as they are not in the g2 fixtures.

--- aliss/tests/views/test_search_view.py
+++ aliss/tests/views/test_search_view.py
@@ -13,6 +13,27 @@ class SearchViewTestCase(TestCase):
         s = Service.objects.create(name="My First Service", description="A handy service", organisation=o, created_by=t, updated_by=u)
         s.service_areas.add(ServiceArea.objects.get(name="Glasgow City", type=2))
 
+        brechin_postcode = Postcode.objects.create(
+            postcode="DD9 6AD", postcode_district="DD9",  postcode_sector="DD3 8",
+            latitude="56.73313937", longitude="-2.65779541",
+            council_area_2011_code="S12000041",
+            health_board_area_2014_code="S08000027",
+            integration_authority_2016_code="S37000003")
+
+        erskine_postcode = Postcode.objects.create(
+            postcode="PA8 7WZ", postcode_district="PA8",  postcode_sector="PA8 7",
+            latitude="55.9054667", longitude="-4.45330031",
+            council_area_2011_code="S12000038",
+            health_board_area_2014_code="S08000021",
+            integration_authority_2016_code="S37000024")
+
+        dundee_postcode = Postcode.objects.create(
+            postcode="DD3 8EA", postcode_district="DD3",  postcode_sector="DD3 8",
+            latitude="56.47774662", longitude="-2.98519045",
+            council_area_2011_code="S12000042",
+            health_board_area_2014_code="S08000027",
+            integration_authority_2016_code="S37000007")
+
     def test_get(self):
         response = self.client.get('/search/?postcode=G2+4AA')
         self.assertEqual(response.status_code, 200)
@@ -36,6 +57,21 @@ class SearchViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<h1>Sorry, ALISS is not available in your postcode.</h1>')
         self.assertContains(response, "gtag('event', 'search-error-unrecognised', {")
+
+    def test_brechin_legacy_url(self):
+        brechin_legacy_url = "/search/?q=&distance=10&latitude=56.73334200000001&longitude=-2.6552888999999595&location=Brechin,+United+Kingdom&page=1"
+        response = self.client.get(brechin_legacy_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_erskine_legacy_url(self):
+        erskine_legacy_url = "/search/?q=&location=Erskine+PA8+7FG,+United+Kingdom&latitude=55.8907926&longitude=-4.444185400000038&distance=15"
+        response = self.client.get(erskine_legacy_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_dundee_legacy_url(self):
+        dundee_legacy_url = "/search/?q=health&accounts=179,91,268&lat=56.47347449999999&lng=-2.957288100000028&location=Dundee%20DD4%207AA,%20United%20Kingdom"
+        response = self.client.get(dundee_legacy_url)
+        self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
         Fixtures.organisation_teardown()
