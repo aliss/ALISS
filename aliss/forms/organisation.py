691f3229ec9234443d8bed69b1a7248fbf88a48b
Mike-Heneghan <m.heneghan@live.co.uk>
2019-03-01 10:25:01 +0000

    Reworded some of the info copy and deleted the logo help text as it does not get rendered by the form partial.

--- aliss/forms/organisation.py
+++ aliss/forms/organisation.py
@@ -23,13 +23,12 @@ class OrganisationForm(forms.ModelForm):
 
         help_texts = {
             'name': _('Please enter the official name of the organisation you would like to add.'),
-            'description': _('Please try to input a clear and concise description of the organisation you would like to add. Keywords can effect your result listing so please add them appropriately.'),
+            'description': _('Please try to input a clear and concise description of the organisation you would like to add. Keywords can effect your listing poistion in the search results so please add them appropriately.'),
             'phone': _('Please add the best telephone number for a user to call if they wish to contact the organisation. '),
-            'email': _('Please enter the best email address to officially contact the organisation you are adding.'),
-            'url': _('Please enter the URL of the official organisation if available in the for https://www.example.com'),
+            'email': _('Please enter the best email address for a user to contact the organisation you are adding.'),
+            'url': _('Please enter the URL of the official organisation webpage if available. Copy the link which should be in the format of https://www.example.com'),
             'facebook': _('Please provide the link to the official Facebook page for the organisation you would like to add.'),
             'twitter': _('Please provide the link to the official Twitter account of the organisation you would like to add'),
-            'logo': _('Please select the image file for the logo of the organisation you would like to add.'),
         }
 
     def clean(self):
