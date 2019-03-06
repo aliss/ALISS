53b097d453a543f8de54016d589f370a5749fbb0
R Gianni <hello@rgianni.cc>
2019-03-06 14:56:35 +0000

    adds headers to tracker

--- aliss/api/views.py
+++ aliss/api/views.py
@@ -36,7 +36,8 @@ class TrackUsageMixin(object):
 
     def send_request(payload):
         try:
-          return requests.post('https://www.google-analytics.com/collect', params=payload)
+            headers = { 'User-Agent': 'ALISS Production' }
+          return requests.post('https://www.google-analytics.com/collect', params=payload, headers=headers)
         except:
           return None
 
