c94a03d6973b0b03d98fd0aa3a1663336bd940e3
R Gianni <hello@rgianni.cc>
2019-04-05 23:35:25 +0100

    dont use split postcode

--- aliss/management/commands/import_places.py
+++ aliss/management/commands/import_places.py
@@ -7,7 +7,7 @@ from aliss.models import Postcode
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        r = requests.get('https://gist.githubusercontent.com/digitalWestie/7fc02a45070bd862ea30cf229d162f21/raw/606af18ca363558135d3952b9d793bfb8cac3088/the-merged-dirty.csv')
+        r = requests.get('https://gist.githubusercontent.com/digitalWestie/7fc02a45070bd862ea30cf229d162f21/raw/6753e636de7b08d15e1555bc263809d94e26c418/the-merged-dirty.csv')
         if r.status_code != 200:
             self.stdout.write(self.style.ERROR('Quitting, status code: ' + r.status_code))
             return None
@@ -17,7 +17,8 @@ class Command(BaseCommand):
         reader = csv.reader(buff)
 
         for row in reader:
-            place = { 'locality': row[2], 'postcode': row[5] }
+            place = { 'use_osm': row[1] == "True", 'locality': row[2], 'osm_postcode':row[4], 'postcode': row[5] }
+
             if place['postcode'] == 'Postcode':
                 continue
                 header = True
@@ -25,7 +26,10 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.ERROR('Quitting, wrong header: ' + place['postcode']))
                 return None
             try:
-                p = Postcode.objects.get(pk=place['postcode'])
+                ps = place['postcode']
+                if place["use_osm"]:
+                    ps = place['osm_postcode']
+                p = Postcode.objects.get(pk=ps)
                 p.place_name = place['locality']
                 p.save()
                 self.stdout.write(self.style.SUCCESS('Successfully updated postcode ' + p.place_name))
