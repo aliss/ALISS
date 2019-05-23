c346a8680b4d4185e0333ced830cd2034a94addd
Mike-Heneghan <m.heneghan@live.co.uk>
2019-05-23 14:53:30 +0100

    Confirmed that the slug field can take a default method but can't be linked to the model instance.

--- aliss/search_tasks.py
+++ aliss/search_tasks.py
@@ -158,3 +158,18 @@ def delete_index():
     connection = _get_connection()
     connection.indices.delete('search', ignore=404)
     connection.indices.delete('organisation_search', ignore=404)
+
+def create_place_name_slugs(force=False):
+    connection = _get_connection()
+    postcodes = Postcode.objects
+
+    if force:
+        postcodes = postcodes.all()
+
+    else:
+        postcodes = postcodes.filter(slug=None).all()
+
+    print("No. of postcode slugs to update: ", postcodes.count())
+    for p in postcodes:
+        p.generate_place_name_slug(force)
+        p.save()
