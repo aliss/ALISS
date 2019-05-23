cfc451832362072d7b24de4ab34f9c0f683828be
Mike-Heneghan <m.heneghan@live.co.uk>
2019-05-23 15:54:15 +0100

    Adding landing page url to init

--- aliss/urls/__init__.py
+++ aliss/urls/__init__.py
@@ -46,6 +46,7 @@ urlpatterns = [
         TemplateView.as_view(template_name="landing-page-leaders.html"),
         name='landing_page_leaders'
     ),
+    url(r'^landing-page/', include('aliss.urls.landing_page')),
     url(r'^reports/$', ReportsView.as_view(), name='reports'),
     url(r'^account/', include('aliss.urls.account')),
     url(r'^search/', include('aliss.urls.search')),
