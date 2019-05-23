8e4ce39c8722c6aaef51ec00fecc46557f87e2ca
Mike-Heneghan <m.heneghan@live.co.uk>
2019-05-23 12:23:35 +0100

    Working on the slug field for the postcode model. Needed to add defualt value as cannot be none.

--- aliss/models/postcode.py
+++ aliss/models/postcode.py
@@ -14,7 +14,7 @@ class Postcode(models.Model):
     health_board_area_2014_code = models.CharField(max_length=10)
     integration_authority_2016_code = models.CharField(max_length=10)
     place_name = models.CharField(max_length=100, blank=True, null=True, default=None)
-    #slug = models.SlugField()
+    slug = models.SlugField()
 
     def __str__(self):
         s = self.postcode
@@ -56,28 +56,16 @@ class Postcode(models.Model):
         if self.pk and self.place_name:
             result = Postcode.objects.filter(pk=self.pk).values('place_name').first()
             name_changed = (result != None) and (result != self.place_name)
-        if force or name_changed: #or self.slug == None:
+        if force or name_changed or self.slug == None:
             s = slugify(self.place_name)
-            # self.slug = s
+            self.slug = s
+            print(s)
+        else:
+            s = self.postcode
+            self.slug = s
             print(s)
         return False
 
-
-
-'''
-    def generate_slug(self, force=False):
-        name_changed = False
-        if self.pk:
-            result = Service.objects.filter(pk=self.pk).values('name').first()
-            name_changed = (result != None) and (result != self.name)
-        if force or self.slug == None or name_changed:
-            s = slugify(self.name)
-            similar = Service.objects.filter(slug__startswith=s).order_by('updated_on')
-            try:
-                slug_n = int(similar.last().slug.split('-')[-1]) + 1
-            except:
-                slug_n = similar.count()
-            self.slug = s + "-" + str(slug_n)
-            return self.slug
-        return False
-'''
+    def save(self, *args, **kwargs):
+        self.generate_place_name_slug()
+        super(Postcode, self).save(*args, **kwargs)
