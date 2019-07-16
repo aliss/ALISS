cc748afd55f8f95bd6939b3a4bdce46f88b34fd3
R Gianni <hello@rgianni.cc>
2019-07-16 12:07:01 +0100

    adds boundary data and extractor method

---
+++ aliss/management/commands/extract_geodata.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    def all_exist(self, paths):
+        for path in paths:
+            if not os.path.exists(path):
+                print(path + " not found, extracting geodata...")
+                return False
+        return True
+
+    def handle(self, *args, **options):
+        boundaries_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data/boundaries'))
+        boundaries_zip = boundaries_dir + '/boundaries.zip'
+        boundary_paths = [
+            boundaries_dir + '/scottish_local_authority.json',
+            boundaries_dir + '/SG_NHS_HealthBoards_2019.json',
+            boundaries_dir + '/SG_NHS_IntegrationAuthority_2019.json'
+        ]
+        if self.all_exist(boundary_paths):
+            print("Boundary data already extracted.")
+        else:
+            print("Extracting " + boundaries_zip + " to " + boundaries_dir)
+            with zipfile.ZipFile(boundaries_zip,"r") as zip_ref:
+                zip_ref.extractall(boundaries_dir)
+            print("Done.")
+
+
