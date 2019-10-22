26a7e77287c894f4cc154da14854eccfcd651743
R Gianni <hello@rgianni.cc>
2019-10-22 13:09:20 +0100

    fix order of urls

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^djga/', include('google_analytics.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('aliss.api.urls')),
     url(r'^', include('aliss.urls')),
-    url(r'^djga/', include('google_analytics.urls')),
 ]
