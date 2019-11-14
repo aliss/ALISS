bce04719043648ebf18e767ffc4110f153b7a9f9
Mike-Heneghan <m.heneghan@live.co.uk>
2019-11-14 14:18:47 +0000

    Cleared up naming error and relared field search.

--- aliss/admin/claim.py
+++ aliss/admin/claim.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.db import models
 from aliss.models import Claim
 
 @admin.register(Claim)
-class CategoryAdmin(admin.ModelAdmin):
+class ClaimAdmin(admin.ModelAdmin):
     list_filter = ['status']
     list_display = ['user', 'organisation', 'status']
-    search_fields = ['user']
+    search_fields = ['user__email', 'user__name']
