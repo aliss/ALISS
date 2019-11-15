3b3c9143ea6d5eae3b9b25a63b6ef398c3a65f0f
Mike-Heneghan <m.heneghan@live.co.uk>
2019-11-15 11:30:22 +0000

    Added a filter to get the potsocde closest to the centre point in each quadrant and then make a dictionary of the postcode and their lng, lats as Point objects.

--- aliss/models/postcode.py
+++ aliss/models/postcode.py
@@ -3,6 +3,8 @@ from django.db import models
 from aliss.models import ServiceArea
 from django.db.models import Avg
 from django.utils.text import slugify
+from django.contrib.gis.geos import Point
+import logging
 
 class Postcode(models.Model):
     postcode = models.CharField(primary_key=True, max_length=9)
@@ -40,26 +42,66 @@ class Postcode(models.Model):
         return ServiceArea.objects.filter(type=2, code=self.council_area_2011_code).first()
 
     def get_by_district(district):
-        try:
+        # try:
+            # Calculate the centroid of the postcode district.
             lng_avg = Postcode.objects.filter(postcode_district=district).aggregate(Avg('longitude'))
             lat_avg = Postcode.objects.filter(postcode_district=district).aggregate(Avg('latitude'))
+            logger = logging.getLogger(__name__)
+            logger.error('Avgs')
+            logger.error(lng_avg)
+            logger.error(lat_avg)
+            centroid = Point(lng_avg['longitude__avg'], lat_avg['latitude__avg'])
+            logger.error(centroid)
+            # closest_postcodes = []
             #TODO: avg between lng and lat ordering
-            lte_postcodes = Postcode.objects.filter(
+            '''
+            Could create an array of arrays where each element is the postcode and the second is it's distance to the centroid and then order by distances ascending and select the first item?
+            '''
+            postcodes_and_distances = {}
+
+            lng_lat_quadrants = []
+
+            lng_gte_lat_gte = Postcode.objects.filter(
+                postcode_district=district,
+                longitude__gte=lng_avg['longitude__avg'],
+                latitude__gte=lat_avg['latitude__avg']
+            ).order_by('longitude', 'latitude').first()
+
+
+            lng_lte_lat_lte = Postcode.objects.filter(
                 postcode_district=district,
                 longitude__lte=lng_avg['longitude__avg'],
                 latitude__lte=lat_avg['latitude__avg']
-            ).order_by('-longitude', '-latitude')
-            if (lte_postcodes.count() > 0):
-                return lte_postcodes.first()
-            else:
-                gte_postcodes = Postcode.objects.filter(
-                    postcode_district=district,
-                    longitude__gte=lng_avg['longitude__avg'],
-                    latitude__gte=lat_avg['latitude__avg']
-                ).order_by('longitude', 'latitude')
-                return gte_postcodes.first()
-        except:
-            raise self.model.DoesNotExist("%s matching query does not exist." % self.model._meta.object_name)
+            ).order_by('-longitude', '-latitude').first()
+
+            lng_gte_lat_lte = Postcode.objects.filter(
+                postcode_district=district,
+                longitude__gte=lng_avg['longitude__avg'],
+                latitude__lte=lat_avg['latitude__avg']
+            ).order_by('longitude', '-latitude').first()
+
+            lng_lte_lat_gte = Postcode.objects.filter(
+                postcode_district=district,
+                longitude__lte=lng_avg['longitude__avg'],
+                latitude__gte=lat_avg['latitude__avg']
+            ).order_by('-longitude', 'latitude').first()
+
+            lng_lat_quadrants.append(lng_gte_lat_gte)
+            lng_lat_quadrants.append(lng_lte_lat_lte)
+            lng_lat_quadrants.append(lng_gte_lat_lte)
+            lng_lat_quadrants.append(lng_lte_lat_gte)
+
+            logger.error(lng_lat_quadrants)
+
+            for quadrant in lng_lat_quadrants:
+                if quadrant is not None:
+                    postcodes_and_distances[quadrant] = Point(quadrant.longitude, quadrant.latitude)
+            logger.error(postcodes_and_distances)
+
+            return(lng_lat_quadrants[0])
+
+        # except:
+        #     raise self.model.DoesNotExist("%s matching query does not exist." % self.model._meta.object_name)
 
     def generate_place_name_slug(self):
         if self.place_name:
