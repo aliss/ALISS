4beb31b501fa8b0254e6c620d386cff85674b225
Mike-Heneghan <m.heneghan@live.co.uk>
2020-01-10 17:27:04 +0000

    Updated the date picker to submit date strings in a format Django can handle.

--- aliss/forms/service.py
+++ aliss/forms/service.py
@@ -71,8 +71,9 @@ class ServiceForm(forms.ModelForm):
         service_areas = cleaned_data.get("service_areas")
         import logging
         logger = logging.getLogger(__name__)
-        logger.error("Test Date")
-        logger.error(cleaned_data.get("start_date_submit"))
+        logger.error("Form")
+        logger.error(cleaned_data.get("start_date"))
+        logger.error(cleaned_data.get("end_date"))
         if (locations.count() == 0) and (service_areas.count() == 0):
             raise forms.ValidationError('Please provide a location and/or a service area for this service.')
         return cleaned_data
