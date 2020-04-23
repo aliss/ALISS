a2dc4377664e2c5130f1795d751f71d129ffb00e
Stefano <slabia@tactuum.com>
2020-04-23 09:35:03 +0100

    security

--- config/settings/base.py
+++ config/settings/base.py
@@ -53,12 +53,26 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+# CORS Config
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = False
+CORS_ALLOW_CREDENTIALS = False
+# CORS_ORIGIN_WHITELIST = False
 CORS_URLS_REGEX = r'^/api/.*$'
 
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
