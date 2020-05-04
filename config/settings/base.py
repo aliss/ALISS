291302cf12d0d224ae58e8e842455103dd3d4b16
Stefano <slabia@tactuum.com>
2020-05-04 15:46:52 +0100

    no message

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,8 +21,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap_admin', 
-    'django-admin-bootstrapped',
+    'django_bootstrap_admin', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sitemaps',
     'django_admin_listfilter_dropdown',
+    'django_admin_bootstrapped',
 
     'django_filters',
     'rest_framework',
