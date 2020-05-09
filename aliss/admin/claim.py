8d601343ee49bb78a883c6b7c4affc9de2ea6d9e
Stefano <slabia@tactuum.com>
2020-05-09 16:55:21 +0100

    mix

--- aliss/admin/claim.py
+++ aliss/admin/claim.py
@@ -2,8 +2,7 @@ import csv
 from django.contrib import admin
 from django.http import HttpResponse
 from django.db import models
-from aliss.models import Claim, Organisation
-
+from aliss.models import Claim, Organisation, ALISSUser
 
 
 class ExportCsvMixin:
@@ -13,11 +12,14 @@ class ExportCsvMixin:
         meta = self.model._meta
        
         field = ['organisation', 'organisation_id' ]
+  
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+    
         writer = csv.writer(response)
         writer.writerow(queryset)      
         for obj in queryset:
+
             row = writer.writerow([getattr(obj, field) for field in field])        
         return response
         
@@ -28,6 +30,8 @@ class ExportCsvMixin:
 class ClaimAdmin(admin.ModelAdmin, ExportCsvMixin):
     
     list_filter = ['status', 'created_on']
-    list_display = ['organisation', 'status', 'created_on']
+    list_display = ['organisation', 'organisation_id', 'status', 'created_on']
+    def organisation_id(self, obj):
+            return obj.organisation.claimed_by
     search_fields = ['organisation__name']
     actions = ['export_as_csv']
