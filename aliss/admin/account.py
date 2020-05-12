5c587e3e58a37086070e80eceeee3b52728bf594
Stefano <slabia@tactuum.com>
2020-05-12 16:28:12 +0100

    user fix

--- aliss/admin/account.py
+++ aliss/admin/account.py
@@ -11,15 +11,15 @@ admin.site.index_title = "Welcome to Aliss admin Portal"
 class ExportCsvMixin:
     def export_as_csv(self, request, queryset):
         meta = self.model._meta
-        field_names = ['name',  'email']
+        fields = ['name',  'email']
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
         writer = csv.writer(response)
-        writer.writerow(field_names)      
-        
+       
         for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])        
+            row = writer.writerow([getattr(obj, field) for field in fields])        
         return response
+        
     export_as_csv.short_description = "Export claimed user"
 
 @admin.register(ALISSUser)
