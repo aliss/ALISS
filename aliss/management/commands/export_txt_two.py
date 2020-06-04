09a716fa0acc4369fca1748e0ff2423f6d2b38e1
Stefano <slabia@tactuum.com>
2020-06-04 23:09:39 +0100

    local_txt_export

--- aliss/management/commands/export_txt_two.py
+++ aliss/management/commands/export_txt_two.py
@@ -2,8 +2,8 @@ import os.path
 import sys
 from django.core import management
 
-local_exp = management.call_command("generate_location_report")
 
-
+class Command(BaseCommand):  
+ local_exp = management.call_command("generate_location_report")
 with open('location.txt', 'w') as f:
     sys.stdout = f
