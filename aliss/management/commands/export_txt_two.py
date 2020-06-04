d9b0508d0cc587900d0c69e9737071bea4c1fb56
Stefano <slabia@tactuum.com>
2020-06-04 21:54:13 +0100

    location_report

--- aliss/management/commands/export_txt_two.py
+++ aliss/management/commands/export_txt_two.py
@@ -3,5 +3,5 @@ from django.core import management
 
 location_export = management.call_command("generate_location_report")
 
-sys.stdout = open("static/pdf/location.txt", "w")
+sys.stdout = open("static/location/location.txt", "w")
 print ("location_export sys.stdout")
