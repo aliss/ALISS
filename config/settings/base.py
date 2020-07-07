d30c2ced3e1c84034ab47b7a8500dfa8390fe88a
Stefano Labia <>
2020-07-07 09:42:20 +0100

    only backend

--- config/settings/base.py
+++ config/settings/base.py
@@ -99,7 +99,7 @@ CORS_ALLOW_METHODS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'aliss/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'aliss/frontend/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -152,7 +152,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/template/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
