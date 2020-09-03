150a36b4621ec22e14734835cea02eea89594f16
Stefano Labia <>
2020-09-03 10:23:46 +0100

    base fixed for the ssl

--- config/settings/base.py
+++ config/settings/base.py
@@ -63,7 +63,6 @@ CORS_ORIGIN_WHITELIST = [
     "http://alissapp.azurewebsites.net/"
 ]
 
-SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CORS_URLS_REGEX = r'^/api/.*$'
