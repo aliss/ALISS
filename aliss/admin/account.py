3289b66680781a3d08075dcb985eccb28ebd09ef
Stefano Labia <>
2020-09-24 13:43:10 +0100

    account search

--- aliss/admin/account.py
+++ aliss/admin/account.py
@@ -29,6 +29,8 @@ class ALISSUserAdmin(admin.ModelAdmin, ExportCsvMixin):
          def __new__(cls, *args, **kwargs):
           search_fields = ['email']
     list_display = ['email', 'name', 'is_editor', 'is_staff', 'date_joined']
+    search_fields = ['email']
+    search_fields = ['name']
     ordering = ['-date_joined', ]
     list_filter = ('is_editor'),
     actions = ['export_as_csv']
