c9a00d1be96c780dc375766c695088ac2186bf92
Stefano Labia <>
2021-01-19 11:40:35 +0000

    api deployed

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,10 +13,10 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
  
-EMAIL_HOST_USER = os.environ['apikey']
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['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']
+EMAIL_HOST_PASSWORD = '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'
  
 SECURE_SSL_REDIRECT = True
