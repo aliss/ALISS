76bcc564baa9e63885c2522ebd7b9f666251227c
Stefano Labia <>
2021-01-19 11:23:13 +0000

    production api mail

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,22 +1,22 @@
 from .base import *
-
+ 
 DEBUG = False
-
+ 
 # Elasticsearch
 ELASTICSEARCH_URL = os.environ.get('FOUNDELASTICSEARCH_URL')
 ELASTICSEARCH_USERNAME = os.environ.get('ELASTICSEARCH_USERNAME', '')
 ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD', '')
-
+ 
 # Temp session engine until we set up Redis
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-
+ 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+ 
+EMAIL_HOST_USER = os.environ['apikey']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
+EMAIL_HOST_PASSWORD = os.environ['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']
+ 
 SECURE_SSL_REDIRECT = True
