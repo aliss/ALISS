0aeb73461721405df3887d34ebb8ac85ad43d29c
Stefano Labia <>
2021-01-19 11:58:19 +0000

    restore API

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,22 +1,22 @@
 from .base import *
- 
+
 DEBUG = False
- 
+
 # Elasticsearch
 ELASTICSEARCH_URL = os.environ.get('FOUNDELASTICSEARCH_URL')
 ELASTICSEARCH_USERNAME = os.environ.get('ELASTICSEARCH_USERNAME', '')
 ELASTICSEARCH_PASSWORD = os.environ.get('ELASTICSEARCH_PASSWORD', '')
- 
+
 # Temp session engine until we set up Redis
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
- 
+
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
- 
-EMAIL_HOST_USER = 'apikey'
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = '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'
- 
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
 SECURE_SSL_REDIRECT = True
