cca7e3b56ce1c8ee3ee5847a01c43eaa1ed7f7f9
Stefano <pasinoe@gmail.com>
2021-03-03 16:59:42 +0000

    production change

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,17 +14,10 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-# EMAIL_HOST_USER = EMAIL_HOST_USER = 'apikey'
-# EMAIL_HOST= 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
-# Twilio SendGrid
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ['apikey']
+EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_API_KEY']
 
 SECURE_SSL_REDIRECT = True
