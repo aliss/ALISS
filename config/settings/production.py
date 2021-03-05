f8ac47f546a5a12b409f3bfad3fb56da7f30f058
Stefano <pasinoe@gmail.com>
2021-03-05 10:18:43 +0000

    mail env

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,10 +13,10 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_PASSWORD = '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'
 
 SECURE_SSL_REDIRECT = True
